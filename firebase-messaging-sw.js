241d9595fd5bbe288c33c24b28cdcf3f62a2648a
Igor <bess87android@gmail.com>
2025-04-30 18:09:13 +0300

    test

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,14 @@
+importScripts("https://www.gstatic.com/firebasejs/11.6.1/firebase-app-compat.js")
+importScripts("https://www.gstatic.com/firebasejs/11.6.1/firebase-messaging-compat.js")
+
+firebase.initializeApp({
+    apiKey: "AIzaSyCPonBWx1ACs0If9h8ZMmxhEGwa5Folccc",
+    authDomain: "trusteesecurity-demo.firebaseapp.com",
+    projectId: "trusteesecurity-demo",
+    storageBucket: "trusteesecurity-demo.firebasestorage.app",
+    messagingSenderId: "573985387120",
+    appId: "1:573985387120:web:738af0b8666787b64efab5",
+    measurementId: "G-KCM5K35T6V"
+});
+
+messaging = firebase.messaging();

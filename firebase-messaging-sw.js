1cee98034a57ac0b63980ceeab3eed50a7e375a3
Igor <bess87android@gmail.com>
2025-04-30 16:12:16 +0300

    test

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,8 +1,8 @@
 // import { initializeApp } from "firebase/app";
 // import { getMessaging, onBackgroundMessage } from "firebase/messaging/sw";
 
-importScripts("https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js")
-importScripts("https://www.gstatic.com/firebasejs/11.6.1/firebase-messaging.js")
+importScripts("https://www.gstatic.com/firebasejs/11.6.1/firebase-app-compat.js")
+importScripts("https://www.gstatic.com/firebasejs/11.6.1/firebase-messaging-compat.js")
 
 
 const firebaseApp = initializeApp({

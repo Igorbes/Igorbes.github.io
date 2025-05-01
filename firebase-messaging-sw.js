ba66b2dc1da47a8a22d222550bd6f4918ae9c8f2
Igor <bess87android@gmail.com>
2025-05-01 21:23:23 +0300

    test

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -13,16 +13,16 @@ firebase.initializeApp({
 
 messaging = firebase.messaging();
 messaging.onBackgroundMessage((payload) => {
-    // console.log('[firebase-messaging-sw.js] Received background message ', payload);
-    // // Customize notification here
-    // const notificationTitle = 'Background Message Title';
-    // const notificationOptions = {
-    //     body: 'Background Message body.',
-    //     icon: '/firebase-logo.png'
-    // };
-    //
-    // self.registration.showNotification(notificationTitle,
-    //     notificationOptions);
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    // Customize notification here
+    const notificationTitle = 'Background Message Title';
+    const notificationOptions = {
+        body: 'Background Message body.',
+        icon: '/firebase-logo.png'
+    };
+
+    self.registration.showNotification(notificationTitle,
+        notificationOptions);
 });
 
 

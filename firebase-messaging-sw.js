31e4b310c68241634e600f59db950004af8af0be
Igor <bess87android@gmail.com>
2025-05-01 21:19:46 +0300

    test

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -13,14 +13,30 @@ firebase.initializeApp({
 
 messaging = firebase.messaging();
 messaging.onBackgroundMessage((payload) => {
-    console.log('[firebase-messaging-sw.js] Received background message ', payload);
-    // Customize notification here
-    const notificationTitle = 'Background Message Title';
-    const notificationOptions = {
-        body: 'Background Message body.',
-        icon: '/firebase-logo.png'
-    };
+    // console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    // // Customize notification here
+    // const notificationTitle = 'Background Message Title';
+    // const notificationOptions = {
+    //     body: 'Background Message body.',
+    //     icon: '/firebase-logo.png'
+    // };
+    //
+    // self.registration.showNotification(notificationTitle,
+    //     notificationOptions);
+});
 
-    self.registration.showNotification(notificationTitle,
-        notificationOptions);
+
+self.addEventListener("notificationclick", (event) => {
+    console.log("On notification click: ", event.notification);
+    // event.waitUntil(
+    //     clients.matchAll({type: 'window'})
+    //         .then(clients => clients.filter(client => client.url === originalUrl))
+    //         .then(matchingClients => {
+    //             if (matchingClients[0]) {
+    //                 return matchingClients[0].navigate(navigationUrl).then(client => client.focus());
+    //             }
+    //
+    //             return clients.openWindow(navigationUrl);
+    //         })
+    // );
 });
